ff6abc8e817d14295048406bec1d29e8f3d9a713
PiotrSter <154370@student.uwm.edu.pl>
2021-10-13 20:43:03 +0200

    floorspawner continue

--- ColoredSquare/Assets/Scripts/FloorAndWall/FloorSpawner.cs
+++ ColoredSquare/Assets/Scripts/FloorAndWall/FloorSpawner.cs
@@ -6,13 +6,18 @@ public class FloorSpawner : MonoBehaviour
 {
     public GameObject floor;
     public Transform player;
-    public int howManyFloor = 0, i = 0;
+    public int howManyFloor = 0;
+    public float pastX = 0, pastY = 0;
     void Awake()
     {
         player = GameObject.Find("Player").GetComponent<Transform>();
         SpawnFloor();
         SpawnFloor();
         SpawnFloor();
+
+        /*
+         Tu trzeba bedize chyba dodawaæ platformy do listy i sprawdzaæ, czy wspólrzedne tych platform nie nachodz¹ na siebie jeœli tak to jes przesuwaæ
+         */
     }
 
     void Update()
@@ -22,8 +27,26 @@ public class FloorSpawner : MonoBehaviour
 
     void SpawnFloor()
     {
-        float x = Random.Range(-8f, 8f), y = Random.Range(player.transform.position.y + 1, player.transform.position.y + 5), sizeX = Random.Range(1f, 8.5f);
+        float randomX = Random.Range(-8f, 8f), randomY = Random.Range(player.transform.position.y + 1, player.transform.position.y + 5), sizeX = Random.Range(1f, 8.5f), floorHeight = floor.transform.localScale.y,
+            floorWidth = floor.transform.localScale.x, x, y = 0; 
         floor.transform.localScale = new Vector3(sizeX, floor.transform.localScale.y, floor.transform.localScale.z);
-        Instantiate(floor, new Vector3(x, y, 0), Quaternion.identity);
+        float beginningOfTheRangeY = pastY - floorHeight, endOfTheRangeY = pastY + floorHeight, beginningOfTheRangeX = pastX - floorWidth, endOfTheRangeX = pastX + floorWidth;
+        if (randomY >= beginningOfTheRangeY && randomY <= endOfTheRangeY)
+        {
+            y = randomY + floorHeight * 2;
+            Instantiate(floor, new Vector3(randomX, y, 0), Quaternion.identity);
+            pastX = randomX;
+            pastY = y;
+        }
+        /* else if (x >= beginningOfTheRangeX && x <= endOfTheRangeX && (y < beginningOfTheRangeY || y > endOfTheRangeY))
+             Instantiate(floor, new Vector3(x + floorHeight * 2, y, 0), Quaternion.identity);
+         else if (x >= beginningOfTheRangeX && x <= endOfTheRangeX && y >= beginningOfTheRangeY && y <= endOfTheRangeY)
+             Instantiate(floor, new Vector3(x + floorHeight * 2, y + floorHeight * 2, 0), Quaternion.identity);*/
+        else
+        {
+            Instantiate(floor, new Vector3(randomX, randomY, 0), Quaternion.identity);
+            pastX = randomX;
+            pastY = randomY;
+        }
     }
 }
